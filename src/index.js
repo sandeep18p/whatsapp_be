e4fec4ea321ea6a92bfe2f245d0dce5702c4a724
sandeep18 <sandeep.pansari98@gmail.com>
2023-12-29 14:04:09 +0530

    setup express nodemon

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// File: index.mjs
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`);
+});
